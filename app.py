5cd17c25d24ac9d461b6126023f56236303aa99f
Les Pounder <lespounder@gmail.com>
2017-07-31 16:20:48 +0000

    Tested, cleaned up and all good!

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
-from gpiozero import LED
+from gpiozero import LED, Motor
+from time import sleep
 
 led = LED(4)
+motor = Motor(forward=17, backward=27)
 
 app = Flask(__name__)
 
@@ -19,6 +21,19 @@ def off():
     led.off()
     return render_template('index.html')
          
+@app.route('/motor-forwards/')
+def motorforwards():
+    motor.forward()
+    sleep(1)
+    motor.stop()
+    return render_template('index.html')
+
+@app.route('/motor-backwards/')
+def motorbackwards():
+    motor.backward()
+    sleep(1)
+    motor.stop()
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
